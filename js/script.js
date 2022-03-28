b1ae34981294236ca3b94d6cb79eae112c795119
mans-woxberg-nti-johanneberg <94618306+mans-woxberg-nti-johanneberg@users.noreply.github.com>
2022-03-28 10:09:49 +0200

    burgarmey

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-// Denna fil skall innehålla JavaScript (JS)
+const nav = document.querySelector('.navlinks')
+const burger = document.querySelector('.burger')
+
+function togglemeny() {
+    nav.classList.toggle('navactive')
+    burger.classList.toggle('toggle')
+}
+
+burger.addEventListener('click', togglemeny)
